15a563e12a3d480126673132cf29fc624b3a0ccc
Ishayat <ishayataronno.13@gmail.com>
2024-12-11 01:31:53 +0600

    User & Post db created

--- main.py
+++ main.py
@@ -1,6 +1,42 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime,timezone
 
 app = Flask(__name__)
+
+# MySQL database configuration
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:admin123@localhost/opendesk'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# db initialization
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    institute = db.Column(db.String(120), nullable=False)
+    department = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
+class Post(db.Model):
+    __tablename__ = 'post'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)  # Foreign key linking to User
+    like_count = db.Column(db.Integer, default=0, nullable=False)
+    comment_count = db.Column(db.Integer, default=0, nullable=False)
+    view_count = db.Column(db.Integer, default=0, nullable=False)
+    user = db.relationship('User', backref=db.backref('posts', lazy=True))
+
+    def __repr__(self):
+        return f'<Post {self.title}>'
 
 @app.route('/')
 def home():
@@ -14,5 +50,11 @@ def signup():
 def login():
     return render_template("login.html")
 
+
+# Temporary table creation
+# with app.app_context():
+#     db.create_all()
+#     print("Tables created!")
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5555,debug=True)
